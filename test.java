4226e65a29e7adfab581bdd35abc925096f6c44e
Pooja Prakash Belgaumkar <94463593+PoojaPrakash08@users.noreply.github.com>
2026-01-09 20:42:05 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,9 @@
+public class HelloJava {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("Hello, Java! Count: " + i);
+        }
+
+    }
+}
